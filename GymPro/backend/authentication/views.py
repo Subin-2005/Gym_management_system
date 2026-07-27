2b9de866aabc90171a7a497ec6810a449372f466
Subin-2005 <subinsr.developer@gmail.com>
2026-07-27 15:25:19 +0530

    authentication

--- GymPro/backend/authentication/views.py
+++ GymPro/backend/authentication/views.py
@@ -11,43 +11,63 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import LoginSerializer
 
 
-class LoginAPIView(APIView):
+# class LoginAPIView(APIView):
 
+#     permission_classes = []
+#     authentication_classes = []
+
+#     def post(self, request):
+
+#         serializer = LoginSerializer(data = request.data)
+
+#         serializer.is_valid(raise_exception = True)
+
+#         username = serializer.validated_data["username"]
+#         password = serializer.validated_data["password"]
+
+#         user = authenticate(
+#             username = username,
+#             password = password
+#         )
+
+#         if user is None:
+
+#             return Response(
+#                 {
+#                     "message": "Invalid Username or Password"
+#                 },
+#                 status = status.HTTP_401_UNAUTHORIZED
+#             )
+
+#         refresh = RefreshToken.for_user(user)
+
+#         return Response({
+#             "access": str(refresh.access_token),
+#             "refresh": str(refresh),
+
+#             "user": {
+#                 "id": user.id,
+#                 "username": user.username,
+#                 "role": user.role,
+#             }
+#         })
+
+
+from django.contrib.auth import get_user_model
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+User = get_user_model()
+
+class CreateAdminAPIView(APIView):
     permission_classes = []
     authentication_classes = []
 
-    def post(self, request):
-
-        serializer = LoginSerializer(data = request.data)
-
-        serializer.is_valid(raise_exception = True)
-
-        username = serializer.validated_data["username"]
-        password = serializer.validated_data["password"]
-
-        user = authenticate(
-            username = username,
-            password = password
-        )
-
-        if user is None:
-
-            return Response(
-                {
-                    "message": "Invalid Username or Password"
-                },
-                status = status.HTTP_401_UNAUTHORIZED
+    def get(self, request):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="Admin@123"
             )
-
-        refresh = RefreshToken.for_user(user)
-
-        return Response({
-            "access": str(refresh.access_token),
-            "refresh": str(refresh),
-
-            "user": {
-                "id": user.id,
-                "username": user.username,
-                "role": user.role,
-            }
-        })
+        return Response({"message": "Admin created"})
